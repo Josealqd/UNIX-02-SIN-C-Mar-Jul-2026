36f9eca8fede268f42d22a4545533db89550d9e4
Josealqd <joalmeidaan@uide.edu.ec>
2026-03-27 16:48:47 +0000

    test.txt

--- filesystem_scrip.sh
+++ filesystem_scrip.sh
@@ -30,3 +30,19 @@ cat saludo_bin
 cd /home 
 #Print the original file content, not the binary
 saludo_bin 
+#It helps me know where I'm moving around.
+pwd
+#change back to the root directory
+cd /
+#list files and directories with symbols to identify types (/, *)
+ls -F
+#list the contents of the current directory
+ls
+#create a file named test.txt with the string "Hola mundo"
+echo "Hola mundo" > test.txt
+#list specific files/directories named java, nvm, and test.txt
+ls java  nvm  test.txt
+#display the content of the test.txt file
+cat test.txt
+#show detailed status and metadata of the test.txt file
+stat test.txt
