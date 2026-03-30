956b2f427a619b55fc7b28af90deef674084cb6a
Josealqd <joalmeidaan@uide.edu.ec>
2026-03-30 15:40:46 +0000

    wip_verify

--- Fs_exploration.sh
+++ Fs_exploration.sh
@@ -2,9 +2,11 @@
 sudo apt update
 #Refreshes the local package index
 sudo apt upgrade
-#
+#Open the terminal and make sure the package manager in up to update
 sudo apt install parted
 #Install numpy
 pip install numpy
 #List all disks and their partitions and display the storage devices in a tree structure.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
+#Verify if we using UEFI
+[ -d /sys/firmware/efi ] && echo "UEFI" || echo "BIOS"
