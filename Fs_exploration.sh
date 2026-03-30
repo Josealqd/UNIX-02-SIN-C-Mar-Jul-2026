be1f05b3a718ce1715ad7b148c89cd1b9ea1a42a
Josealqd <joalmeidaan@uide.edu.ec>
2026-03-30 15:31:09 +0000

    wip_list

--- Fs_exploration.sh
+++ Fs_exploration.sh
@@ -1,10 +1,10 @@
-#
+#Install Avaliable Updates
 sudo apt update
-#
+#Refreshes the local package index
 sudo apt upgrade
 #
 sudo apt install parted
-#
+#Install numpy
 pip install numpy
-#
+#List all disks and their partitions and display the storage devices in a tree structure.
 sudo parted -l && echo -e "\n---\n" && lsblk -f && echo -e "\n---\n"
