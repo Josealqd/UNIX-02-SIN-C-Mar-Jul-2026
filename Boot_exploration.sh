bfe5f866d5ad0957da542be671a5457829141c81
Josealqd <joalmeidaan@uide.edu.ec>
2026-04-17 14:42:19 +0000

    man ls wip

--- Boot_exploration.sh
+++ Boot_exploration.sh
@@ -2,3 +2,18 @@
 ls -a 
 #Show the files in the folder
 ls --all
+#Individual flags
+ls -l -a -h  
+#Mixed flags
+ls -l -ah
+#Combined flags (The most common way to write it)    
+ls -lah  
+#This command tells the system that "-rf" is a name, not a set of options.
+mkdir -- -rf
+#This removes the folder named "-rf" only if it is empty.
+rmdir -- -rf
+#This command displays a quick-reference manual for the "ls" command.
+ls --help
+#It gives us a complete manual 
+man ls
+#to navigate. I press the "/" key and type "all," and to move down I press the lowercase letter n, and to go up I press uppercase n, and to exit I press Q.
