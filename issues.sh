9a1897ad415b840aa0139748e97b6657ab99e9d2
Josealqd <joalmeidaan@uide.edu.ec>
2026-04-27 15:14:46 +0000

    issues.wip

---
+++ issues.sh
@@ -0,0 +1,3 @@
+#
+Para resolver los problemas que se nos presentan no debemos acudir directamente a la ia ya que hay otros ingenieros que pudieron ya haber pasado
+nuestros mismos problemas y podemos encontrar los repositorios en github o en Stack over flow
