e8472579c34afcb63e76b4545ad6df1f3deaad5c
Josealqd <joalmeidaan@uide.edu.ec>
2026-05-29 15:07:53 +0000

    wip: Linux group management

--- security2.zsh
+++ security2.zsh
@@ -11,3 +11,10 @@ touch ~/test_inherited_group.txt
 ls -la ~/test_inherited_group.txt
 
 # The group is the user's primary group
+# View the current group
+id -gn
+echo "Current group: $(id -gn)"
+
+# Create a file before newgrp
+touch ~/before_newgrp.txt
+ls -la ~/before_newgrp.txt
