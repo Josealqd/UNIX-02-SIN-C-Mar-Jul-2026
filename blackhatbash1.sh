e6331c8a02338ebf183c062f85d67d0871c55aa5
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-01 15:16:09 +0000

    wip: Add execution permissions

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -12,5 +12,12 @@ df --human-readable
 #bash -x blackhatbash1.sh
 #Run the script in debug mode. This displays each command and its arguments in the terminal as they are executed. This is used to find errors.
 #bash -r blackhatbash1.sh
+#Runs the script in your current folder
+#./blackhatbash1.sh
+#Gives you permission to execute it.
+chmod u+x blackhatbash1.sh
+
+
+
 
 
