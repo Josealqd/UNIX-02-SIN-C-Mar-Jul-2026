241cd53676e327317ec4fe60e5f6cd4edec2c247
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-01 15:07:34 +0000

    wip: bash-r and -x

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -8,7 +8,9 @@ echo ${UID}
 echo ${OSTYPE}
 ps -ef
 df --human-readable
-bash -x blackhatbash1.sh
-bash -r blackhatbash1.sh
+#Run the script in restricted mode (limits commands like `cd`, modifying environment variables, or executing commands with `/`). This is used for security.
+#bash -x blackhatbash1.sh
+#Run the script in debug mode. This displays each command and its arguments in the terminal as they are executed. This is used to find errors.
+#bash -r blackhatbash1.sh
 
 
