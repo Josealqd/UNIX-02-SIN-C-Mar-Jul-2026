f6dc416948b3d525e9d2cf3eaec09db57b90d534
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-01 15:44:05 +0000

    wip: echo and use of unset

--- blackhatbash3.sh
+++ blackhatbash3.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bas
+
 book="black hat bash"
 echo "This book's name is ${book}"
+echo "This book's name is $book"
+root_directory=$(ls -ld /)
+echo "${root_directory}
+book="Black Hat Bash"
+unset book
+echo "${book}"
