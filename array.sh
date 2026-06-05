889cadb1c6d0fc4048bb0a70f2ea402ef8be51b3
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-05 14:56:38 +0000

    wip: array 2.0

--- array.sh
+++ array.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 # Sets an array
 IP_ADDRESSES=(192.168.1.1 192.168.1.2 192.168.1.3)
+unset IP_ADDRESSES[1]
+IP_ADDRESSES[0]="192.168.1.10"
 # Prints all elements in the array
 echo "${IP_ADDRESSES[*]}"
-#--------------------------------------------------------
-$ chmod u+x array.sh
-$ ./array.sh
-set -x 
-set +x
-#---------------------------------------------------------
+# Prints only the first element in the array
+echo "${IP_ADDRESSES[0]}"
