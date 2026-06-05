5fc6f48b7cf595ae40b4983c703227ecff0ad378
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-05 15:26:02 +0000

    wip: lzl

--- background.sh
+++ background.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script will send the sleep command to the background.
 echo "Sleeping for 10 seconds..."
-sleep 10 &
+sleep 60 &
 # Creates a file
 echo "Creating the file test123"
 touch test123
