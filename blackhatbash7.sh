31d322b307b07ab8809145214f943f001e354086
Josealqd <joalmeidaan@uide.edu.ec>
2026-06-19 15:14:13 +0000

    wip: use of log

--- blackhatbash7.sh
+++ blackhatbash7.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 touch example_file1 example_file2 example_file3 
+
+grep "35.237.4.214" log.txt
+
+grep "35.237.4.214\|13.66.139.0" log.txt
+
+ps | grep TTY
+ps | grep tty
+ps | grep -i tty
+
+grep -v "35.237.4.214" log.txt
